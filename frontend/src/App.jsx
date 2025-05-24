1b405546ae40e77bcfba117a735ce1f7bc7eba20
Nejc-GM <nejc.gjura-meke@inova.si>
2025-05-24 22:45:53 +0200

    add authentication login/register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
 import NewPlace from "./places/pages/NewPlace";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Authenticate from "./user/pages/Authenticate";
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
             <Route path="/:userId/places" element={<UserPlaces />} />
             <Route path="/places/new" element={<NewPlace />} />
             <Route path="/places/:placeId" element={<UpdatePlace />} />
+            <Route path="/auth" element={<Authenticate />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </div>
