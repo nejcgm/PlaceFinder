ffbb1b6682ea429a9adb43717ac737c319d02c5b
nejcgm <nejc.gjurameke@gmail.com>
2025-05-26 11:25:06 +0200

    add some basic routes, error handeling (#10)
    Co-authored-by: Nejc-GM <nejc.gjura-meke@inova.si>

--- api/server.js
+++ api/server.js
@@ -1,13 +1,22 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const placesRoutes = require('./routes/places-routes');
-const usersRoutes = require('./routes/users-routes');
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
 
- const app = express()
- app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
- app.use(placesRoutes)
+app.use("/api/places", placesRoutes);
 
+app.use("/api/users", usersRoutes);
 
- app.listen(8000);
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
+app.listen(8000);
