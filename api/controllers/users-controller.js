13deba8c41020b175a30a6387267d58564185e41
Nejc-GM <nejc.gjura-meke@inova.si>
2025-05-30 15:41:24 +0200

    implemented jwt token and password hashing to api

--- api/controllers/users-controller.js
+++ api/controllers/users-controller.js
@@ -2,6 +2,9 @@ const HttpError = require("../models/http-error");
 const uuid = require("uuid").v4;
 const { validationResult } = require("express-validator");
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const getAllUsers = async (req, res, next) => {
   let users;
@@ -36,13 +39,33 @@ const register = async (req, res, next) => {
     );
   }
 
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    return next(
+      new HttpError("Hashing password failed, please try again.", 500)
+    );
+  }
+
   const createdUser = new User({
     name,
     email,
     image: req.file.path,
-    password,
+    password: hashedPassword,
     places: [],
   });
+
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: createdUser.id, email: createdUser.email },
+      PRIVATE_KEY,
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    return next(new HttpError("Creating user failed, please try again.", 500));
+  }
 
   try {
     await createdUser.save();
@@ -50,7 +73,9 @@ const register = async (req, res, next) => {
     const error = new HttpError("Creating User failed", 500);
     return next(error);
   }
-  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+  res
+    .status(201)
+    .json({ userId: createdUser.id, email: createdUser.email, token: token });
 };
 
 const logIn = async (req, res, next) => {
@@ -63,15 +88,46 @@ const logIn = async (req, res, next) => {
     return next(new HttpError("login failed try again later", 500));
   }
 
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
+    return next(
+      new HttpError("Invalid credentials, could not log you in.", 401)
+    );
+  }
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    return next(
+      new HttpError(
+        "Could not log you in, please check your credentials and try again.",
+        500
+      )
+    );
+  }
+  if (!isValidPassword) {
     return next(
       new HttpError("Invalid credentials, could not log you in.", 401)
     );
   }
 
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: existingUser.id, email: existingUser.email },
+      PRIVATE_KEY,
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    return next(new HttpError("Login failed, please try again.", 500));
+  }
+
   res.json({
     message: "Logged in successfully!",
-    user: existingUser.toObject({ getters: true }),
+    user: existingUser.toObject({
+      userId: existingUser.id,
+      email: existingUser.email,
+      token: token,
+    }),
   });
 };
 
--- api/routes/places-routes.js
+++ api/routes/places-routes.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 const { check } = require("express-validator");
 const fileUpload = require("../middleware/file-upload");
+const checkAuth = require("../middleware/check-auth");
 
 const placesControllers = require("../controllers/places-controllers");
 
 router.get("/:placeId", placesControllers.getPlaceById);
 
 router.get("/user/:userId", placesControllers.getPlacesByUserId);
+
+router.use(checkAuth);
 
 router.post(
   "/",
